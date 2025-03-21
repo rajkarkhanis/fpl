e4bdf35ae806bcd0fc3fd6030340829e64bb2060
Raj Karkhanis <rkarkhanis00@gmail.com>
2025-03-21 17:02:52 +0530

    add basic player data
    - calls global data endpoint
    - adds player name, team, and position to gw

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ import polars as pl
 
 season = "2024-25"
 
+def fetch_global_data() -> pl.DataFrame:
+    print("Getting global data")
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
 def fetch_gw(gw_num: int) -> pl.DataFrame:
+    print(f"Getting data for gameweek {gw_num}")
     url = f"https://fantasy.premierleague.com/api/event/{gw_num}/live/"
     response = requests.get(url)
     response.raise_for_status()
@@ -17,6 +26,22 @@ def fetch_gw(gw_num: int) -> pl.DataFrame:
 
     return df
 
+def add_player_info(players: pl.DataFrame, teams: pl.DataFrame, positions: pl.DataFrame, gw: pl.DataFrame) -> pl.DataFrame:
+    players = players.select(["id", "first_name", "second_name", "team", "element_type"])
+
+    players = players.with_columns(
+        (players["first_name"] + " " + players["second_name"]).alias("name")
+    ).drop(["first_name", "second_name"])
+
+    teams = teams.select(["id", "name"]).rename({"id": "team", "name": "team_name"})
+    players = players.join(teams, on="team", how="left").drop("team")
+
+    positions = positions.select(["id", "singular_name_short"]).rename({"id": "element_type", "singular_name_short": "position"})
+    players = players.join(positions, on="element_type", how="left").drop("element_type")
+
+    gw = gw.join(players, on="id", how="left")
+    return gw
+
 def save_to_csv(df: pl.DataFrame, gw: int) -> None:
     folder_path = f"data/{season}"
     os.makedirs(folder_path, exist_ok=True)
@@ -27,10 +52,17 @@ def save_to_csv(df: pl.DataFrame, gw: int) -> None:
 
 
 def main(latest_gw: int) -> None:
-    for gw in range(1, latest_gw + 1):
-        df = fetch_gw(gw)
-        save_to_csv(df, gw)
+    global_data = fetch_global_data()
+    players = pl.DataFrame(global_data["elements"])
+    teams = pl.DataFrame(global_data["teams"])
+    positions = pl.DataFrame(global_data["element_types"])
+
+    for gw_num in range(1, latest_gw + 1):
+        gw = fetch_gw(gw_num)
+        print(f"Adding player info for gameweek {gw_num}")
+        gw = add_player_info(players, teams, positions, gw)
+        save_to_csv(gw, gw_num)
 
 if __name__ == "__main__":
-    latest_gw = 3
+    latest_gw = 29
     main(latest_gw)
